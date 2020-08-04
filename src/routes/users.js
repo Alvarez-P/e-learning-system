fb6fcb589a663a119a1b5ba2c0444033708a6354
Victoriasaurio <victoriasaurio.guzmanc@gmail.com>
2020-08-03 22:29:09 -0500

    Ruta get users añadida, junto con sus respectivas validaciones

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
 
-const { userCreateSchema } = require('../schemas/userSchema')
-const validator = require('express-joi-validation').createValidator({passError: true})
-const { addUser } = require('../controllers/users')
+const { userCreateSchema, userGetSchema } = require('../schemas/userSchema')
+const validator = require('express-joi-validation').createValidator({ passError: true })
+const { addUser, getUser } = require('../controllers/users')
 
 router.post('/', validator.body(userCreateSchema), addUser)
+router.get('/', validator.query(userGetSchema), getUser)
 
 module.exports = router
