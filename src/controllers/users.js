6279c92272499b8ddbc6a56bdb32995bdef7ccf6
Victoriasaurio <victoriasaurio.guzmanc@gmail.com>
2020-08-03 22:22:05 -0500

    getUser - Función getUser añadida. - Contiene la función buildUserFilters, que se encarga de almacenar las querys predeterminadas y las otorgadas por el usuario - Contiene la función getUserOnDB, encargada de buscar en la base de datos, la lista señalada por medio de las querys. - Señala la lista total de objetos existentes, así como también el total de objetos desplegados dependiendo del offset y limit.

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,6 +1,7 @@
 const encrypt = require('../utils/encrypt')
 const genrateID = require('../utils/generateID')
-const { addUserOnDB } = require('../db/methods/users')
+const { addUserOnDB, getUserOnDB } = require('../db/methods/users')
+const { buildUserFilters } = require('../db/buildFilters/users');
 
 /**
  * @function addUser
@@ -9,7 +10,7 @@ const { addUserOnDB } = require('../db/methods/users')
  * @param {Object} res - Express response object
  * @param {Function} next - Express middleware function
  */
-const addUser = async (req, res, next) => {
+const addUser = async(req, res, next) => {
     try {
         req.body.UserPassword = encrypt(req.body.UserPassword, 10)
         req.body.UserID = genrateID()
@@ -20,6 +21,32 @@ const addUser = async (req, res, next) => {
     }
 }
 
+/**
+ * @function getUser
+ * @description Controller for GET /api/users
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
+ * @param {Function} next - Express middleware function
+ */
+
+const getUser = async(req, res, next) => {
+    try {
+        let { filters, offset, limit } = buildUserFilters(req.query, next)
+        let listUsers = await getUserOnDB(filters, next)
+        const count = listUsers.length
+        listUsers = listUsers.slice(offset, offset + limit)
+        res.status(200).send({
+            count,
+            currentTotal: listUsers.length,
+            result: listUsers
+        })
+
+    } catch (error) {
+        next(error)
+    }
+}
+
 module.exports = {
-    addUser
+    addUser,
+    getUser
 }
