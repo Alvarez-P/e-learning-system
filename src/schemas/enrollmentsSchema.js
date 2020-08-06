13eb38a68e97931e065499ed8c95507e53659a2c
Paul Mena <e17081416@itmerida.edu.mx>
2020-08-06 18:56:04 -0500

    solucion de campos en los esquemas, falta sasaber si es activo o no

--- src/schemas/enrollmentsSchema.js
+++ src/schemas/enrollmentsSchema.js
@@ -2,11 +2,12 @@ const Joi = require('@hapi/joi')
 
 module.exports = {
     // enrollment schema validator for POST
-  enrollmentCreateSchema: Joi.object({ //investigar doc uuid joi
-    EnrollmentStudentID: Joi.uuid().required(),
-    EnrollmentCourseID: Joi.uuid().required(),
+  enrollmentCreateSchema: Joi.object({ 
+    EnrollmentsID: Joi.string().guid({version: ['uuidv4']}).required(),
+    EnrollmentStudentID: Joi.string().guid({version: ['uuidv4']}).required(),
+    EnrollmentCourseID: Joi.string().guid({version: ['uuidv4']}).required(),
     EnrollmentCostCourse: Joi.number().required(),
     EnrollmentPaidOut: Joi.boolean().required(),
-    EnrollmentActive: Joi.boolean().required() //investigar como poner elemento activo
+    EnrollmentActive: Joi.boolean().required() //investigar como poner elemento activo Joi.boolean().truthy('yes').falsy('no').sensitive().required()
   })
 }
