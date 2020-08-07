92bce2a3880daea6f199e6b38e2d2d8a583992e1
Victoriasaurio <victoriasaurio.guzmanc@gmail.com>
2020-08-07 09:31:45 -0500

    Archivo test corregido - Test unidos en un solo describe. - Códigos de error utilizados en cada caso del test según corresponda.

--- src/tests/user.test.js
+++ src/tests/user.test.js
@@ -22,44 +22,35 @@ describe("POST /users", () => {
 
 describe("GET /users", () => {
     test("It should get Users", done => {
-        request(app)
-            .get("/api/users?offset=0&limit=20&rol=admin")
-            .then(response => {
-                expect(response.statusCode).toBe(200);
-                done();
-            });
-    });
-});
-
-describe("GET /users", () => {
-    test("It should get Users", done => {
-        request(app)
-            .get("/api/users?offset=0&limit=20&rol=suplente")
-            .then(response => {
-                expect(response.statusCode).toBe(200);
-                done();
-            });
-    });
-});
-
-describe("GET /users", () => {
-    test("It should get Users", done => {
-        request(app)
-            .get("/api/users?offset=")
-            .then(response => {
-                expect(response.statusCode).toBe(200);
-                done();
-            });
-    });
-});
-
-describe("GET /users", () => {
-    test("It should get Users", done => {
-        request(app)
-            .get("/api/users?offset=0&limit=20&rol=admin/status=true")
-            .then(response => {
-                expect(response.statusCode).toBe(200);
-                done();
-            });
-    });
+            request(app)
+                .get("/api/users?offset=0&limit=20&rol=admin")
+                .then(response => {
+                    expect(response.statusCode).toBe(200);
+                    done();
+                });
+        }),
+        test("It should get Users", done => {
+            request(app)
+                .get("/api/users?offset=0&limit=20&rol=suplente")
+                .then(response => {
+                    expect(response.statusCode).toBe(400);
+                    done();
+                });
+        }),
+        test("It should get Users", done => {
+            request(app)
+                .get("/api/users?offset=")
+                .then(response => {
+                    expect(response.statusCode).toBe(400);
+                    done();
+                });
+        }),
+        test("It should get Users", done => {
+            request(app)
+                .get("/api/users?offset=0&limit=20&rol=admin/status=true")
+                .then(response => {
+                    expect(response.statusCode).toBe(400);
+                    done();
+                });
+        })
 });
