9c65921651ab3adde6894632dfcdb4972a94f23b
Paul Mena <e17081416@itmerida.edu.mx>
2020-08-06 19:06:50 -0500

    enrollmentsSchema finish

--- src/schemas/enrollmentsSchema.js
+++ src/schemas/enrollmentsSchema.js
@@ -3,7 +3,7 @@ const Joi = require('@hapi/joi')
 module.exports = {
     // enrollment schema validator for POST
   enrollmentCreateSchema: Joi.object({ 
-    EnrollmentsID: Joi.string().guid({version: ['uuidv4']}).required(),
+    EnrollmentID: Joi.string().guid({version: ['uuidv4']}).required(),
     EnrollmentStudentID: Joi.string().guid({version: ['uuidv4']}).required(),
     EnrollmentCourseID: Joi.string().guid({version: ['uuidv4']}).required(),
     EnrollmentCostCourse: Joi.number().required(),
