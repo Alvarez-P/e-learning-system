dc83e72052692a1273cf5e6422f34eda773c2016
Paul Mena <e17081416@itmerida.edu.mx>
2020-08-07 09:54:32 -0500

    models enrollment add & method addEnrollmentOnDB created

--- src/db/models/enrollment.js
+++ src/db/models/enrollment.js
@@ -11,6 +11,16 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Enrollment.associate = function(models) {
+        Enrollment.hasOne(models.EnrollmentStudentID, {
+          foreignKey: 'EnrollmentStudentID',
+          as: 'student'
+        }),
+        Enrollment.hasOne(models.EnrollmentCourseID, {
+          foreignKey: 'EnrollmentCourseID',
+          as: 'course'
+        })
+      }
     }
   };
   Enrollment.init({
