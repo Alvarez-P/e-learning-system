b2db385e966ed56f63cea2b191936c66239d38bc
Victoriasaurio <victoriasaurio.guzmanc@gmail.com>
2020-08-12 15:26:55 -0500

    Nueva ruta agregada - Se agregó el archivo auth.js que contiene la ruta del login

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,14 @@
 const express = require('express')
 const api = express.Router()
 const routerUser = require('./users')
+const routerAuth = require('./auth')
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger.json');
 
 api.use('/docs', swaggerUi.serve);
 api.get('/docs', swaggerUi.setup(swaggerDocument));
 api.use('/users', routerUser)
+api.use('/auth', routerAuth)
 api.get('/', (req, res) => { res.send({ message: 'Home' }); });
 
 module.exports = api
