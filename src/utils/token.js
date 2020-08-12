bf5558474fd108293afc0aac617563a12bd13234
Victoriasaurio <victoriasaurio.guzmanc@gmail.com>
2020-08-12 16:20:06 -0500

    GENERAR TOKEN - Recibe el data(payload) - Recibe el tiempo de expiración del token - Recibe la firma o llave del token - Retorna un token

---
+++ src/utils/token.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+const jwt = require('jsonwebtoken')
+const { SECRET = 'Secret_key' } = process.env
+
+/**
+ * @function token
+ * @description Funtion to generate token
+ * @param {Object} data Contiene el payload del token
+ * @param {Object} time Contiene el tiempo de expiración del token.
+ * @return {String} retorna un token
+ */
+
+function generateToken(data, time) {
+    return jwt.sign({
+        exp: Math.floor((Date.now() / 1000) + (time * 60)),
+        email: data.UserEmail,
+        password: data.UserPassword,
+        rol: data.UserRol
+    }, SECRET)
+}
+
+module.exports = {
+    generateToken
+}
