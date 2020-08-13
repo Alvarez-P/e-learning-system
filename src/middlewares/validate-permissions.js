15e94c6b80131b92f899c007d90577cdbd32b53e
Esteban Alvarez <aeap19980929@gmail.com>
2020-08-12 19:03:56 -0500

    mend

--- src/middlewares/validate-permissions.js
+++ src/middlewares/validate-permissions.js
@@ -1,6 +1,12 @@
 const { getToken, getRol, validateRol } = require('../utils/verify-rol')
 const pipe = require('../utils/pipe')
 
+/**
+ * @function validatePermissions
+ * @description Fcuntion for validate permissions of request
+ * @param {Array} allowed Allowed roles array
+ * @return {Function} Middleware
+ */
 const validatePermissions = (...allowed) => {
     return (request, response, next) => {
         pipe(
