be85e5b52404aa64dc8beb4486978b7d676b572e
Paul Mena <e17081416@itmerida.edu.mx>
2020-08-19 13:53:57 -0500

    roles agregados

--- src/routes/enrollments.js
+++ src/routes/enrollments.js
@@ -4,7 +4,8 @@ const router = express.Router()
 const { enrollmentCreateSchema } = require('../schemas/enrollmentsSchema')
 const validator = require('express-joi-validation').createValidator({passError: true})
 const { addEnrollment } = require('../controllers/enrollments')
+const {validatePermissions} = require('../middlewares/validate-permissions')
 
-router.post('/', validator.body(enrollmentCreateSchema), addEnrollment)
+router.post('/', validatePermissions("admin", "teacher"), validator.body(enrollmentCreateSchema), addEnrollment)
 
 module.exports = router
