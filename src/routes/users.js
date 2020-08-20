be9a9e33db1823903352fe6d19f78c8a7fc94ad7
Victoriasaurio <victoriasaurio.guzmanc@gmail.com>
2020-08-19 22:33:39 -0500

    ruta del GET de usuario por ID

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const router = express.Router()
 
-const { userCreateSchema, userGetSchema } = require('../schemas/userSchema')
+const { userCreateSchema, userGetSchema, userGetByIdSchema } = require('../schemas/userSchema')
 const validator = require('express-joi-validation').createValidator({ passError: true })
-const { addUser, getUser } = require('../controllers/users')
+const { addUser, getUser, getUserById } = require('../controllers/users')
 const { validateIfEmailExists } = require('../middlewares/validate-users')
 const validatePermissions = require('../middlewares/validate-permissions')
 
 router.post('/', validatePermissions(), validator.body(userCreateSchema), validateIfEmailExists, addUser)
 router.get('/', validator.query(userGetSchema), getUser)
+router.get('/:id', validatePermissions('admin'), validator.params(userGetByIdSchema), getUserById)
 
 module.exports = router
