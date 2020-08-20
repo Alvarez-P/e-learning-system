212b8469d916238965e224a0515167dfd73169cf
Victoriasaurio <victoriasaurio.guzmanc@gmail.com>
2020-08-19 22:32:25 -0500

    Punto y coma

--- src/middlewares/errors.js
+++ src/middlewares/errors.js
@@ -5,11 +5,11 @@
  * @param {Object} req - Express request object
  * @param {Object} res - Express response object
  * @param {Function} next - Express next middleware function
-*/
+ */
 const handleErrors = (err, req, res, next) => {
-    let code, error
-    (err.error && err.error.isJoi) ? 
-    (code = 400, error = err.error.toString()) : ({code = 500, error = 'An internal server error ocurred'} = err)
+    let code, error;
+    (err.error && err.error.isJoi) ?
+    (code = 400, error = err.error.toString()) : ({ code = 500, error = 'An internal server error ocurred' } = err)
     res.status(code).send({ error })
 }
 
